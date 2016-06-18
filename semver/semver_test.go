8bec83c430ecbec3013dbc6f910b5b8113be450f
Matthew Andrews <matt@mattandre.ws>
2016-06-18 22:19:33 +0100

    100% coverage for semver

--- semver/semver_test.go
+++ semver/semver_test.go
@@ -1,6 +1,7 @@
 package semver
 
 import (
+	"errors"
 	"github.com/matthew-andrews/go-version"
 	"testing"
 )
@@ -13,6 +14,9 @@ func getVersion(versionString string) *version.Version {
 }
 
 func (source mockSource) VersionsFor(id string) ([]*version.Version, error) {
+	if id == "erroring" {
+		return []*version.Version{}, errors.New("VersionsFor errored for some reason")
+	}
 	return []*version.Version{
 		getVersion("v0.0.1"),
 		getVersion("v0.1.1"),
@@ -25,15 +29,12 @@ func (source mockSource) VersionsFor(id string) ([]*version.Version, error) {
 
 func TestLatest(t *testing.T) {
 	source := mockSource{}
-
 	actual, err := Semver(source, "id", "latest")
 
 	if err != nil {
 		t.Fatalf("unexpected error thrown", err)
 	}
-
 	expected := "v1.1.1"
-
 	if actual != expected {
 		t.Fatalf("latest expected: %s, actual: %s", expected, actual)
 	}
@@ -41,16 +42,52 @@ func TestLatest(t *testing.T) {
 
 func TestTilde(t *testing.T) {
 	source := mockSource{}
-
 	actual, err := Semver(source, "id", "~1.0.0")
 
 	if err != nil {
 		t.Fatalf("unexpected error thrown", err)
 	}
-
 	expected := "v1.0.3"
-
 	if actual != expected {
 		t.Fatalf("latest expected: %s, actual: %s", expected, actual)
+	}
+}
+
+func TestErrorNoVersionsMatchingConstraint(t *testing.T) {
+	source := mockSource{}
+	_, err := Semver(source, "id", "~2.0.0")
+
+	if err == nil {
+		t.Fatalf("error expected to be thrown but none was")
+	}
+	expected := "no version found matching constraint"
+	if err.Error() != expected {
+		t.Fatalf("latest expected: %s, actual: %s", expected, err.Error())
+	}
+}
+
+func TestErrorBadConstraint(t *testing.T) {
+	source := mockSource{}
+	_, err := Semver(source, "id", "BAD CONSTRAINT")
+
+	if err == nil {
+		t.Fatalf("error expected to be thrown but none was")
+	}
+	expected := "Malformed constraint: BAD CONSTRAINT"
+	if err.Error() != expected {
+		t.Fatalf("latest expected: %s, actual: %s", expected, err.Error())
+	}
+}
+
+func TestErrorVersionsForErrored(t *testing.T) {
+	source := mockSource{}
+	_, err := Semver(source, "erroring", "^1.0.0")
+
+	if err == nil {
+		t.Fatalf("error expected to be thrown but none was")
+	}
+	expected := "VersionsFor errored for some reason"
+	if err.Error() != expected {
+		t.Fatalf("latest expected: %s, actual: %s", expected, err.Error())
 	}
 }
